638df8526f80b5d37ff966af3fdef11ad90f2adf
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2023-04-26 09:01:34 +0200

    Add my palce files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my-place
-Share My Place App
+
+ [Share My Place App](https://app.netlify.com/sites/my-palce/settings/general)
+
+> # Using Google Maps Platform
+
+> From course: [JavaScript - The Complete Guide 2023 (Beginner + Advanced)](https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/)
---
+++ practice-places/src/Utility/Location.js
@@ -0,0 +1,35 @@
+import { async } from "regenerator-runtime";
+
+const GOOGLEAPIKEY = "AIzaSyAaN18WwlksEF68UnRIwM0hjnjo0ys1USo";
+
+export async function getAddressFromCoords(coords) {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${GOOGLEAPIKEY}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch address. Please try again!");
+  }
+  const data = await response.json();
+  if (data.error_message) {
+    throw new Error(data.error_message);
+  }
+  const address = data.results[0].formatted_address;
+  return address;
+}
+
+export async function getCoordsFromAddress(address) {
+  const urlAddress = encodeURI(address);
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=1600=${urlAddress}&key=${GOOGLEAPIKEY}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch coordinates. Please try again!");
+  }
+  const data = await response.json();
+  if (data.error_message) {
+    throw new Error(data.error_message);
+  }
+
+  const coordinates = data.results[0].geometry.location;
+  return coordinates;
+}
